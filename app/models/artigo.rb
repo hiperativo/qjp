d2491e30e1575d972bf2f4f271f4bf8704cfbd34
Pedro Maciel <pedrozath@gmail.com>
2013-01-16 15:36:51 -0200

    update

--- app/models/artigo.rb
+++ app/models/artigo.rb
@@ -22,28 +22,28 @@ class Artigo < ActiveRecord::Base
 	end
 
 	rails_admin do
-		weight 0
-		configure :coluna, :belongs_to_association 
-		configure :user, :belongs_to_association
-		configure :author, :belongs_to_association
+		# weight 0
+		# configure :coluna, :belongs_to_association 
+		# configure :user, :belongs_to_association
+		# configure :author, :belongs_to_association
 		
-		configure :assunto, :string do
-			help "Uma palavra que defina o foco desse artigo. Ex: Num artigo sobre o lançamento de uma prequel de sandman, seria apenas 'Sandman'"
-		end
+		# configure :assunto, :string do
+		# 	help "Uma palavra que defina o foco desse artigo. Ex: Num artigo sobre o lançamento de uma prequel de sandman, seria apenas 'Sandman'"
+		# end
 		
-		configure :chamada, :string do 
-			help "O título principal do artigo. Ex: Filme 'O Hobbit' chega nos cinemas em Janeiro de 2013."
-		end
+		# configure :chamada, :string do 
+		# 	help "O título principal do artigo. Ex: Filme 'O Hobbit' chega nos cinemas em Janeiro de 2013."
+		# end
 		
-		configure :resumo, :text do
-			help "Dê um gostinho desse artigo em no máximo 120 caracteres. Algo que chame atenção do leitor. Mais pra frente, isso será utilizado na integração com twitter"
-		end
+		# configure :resumo, :text do
+		# 	help "Dê um gostinho desse artigo em no máximo 120 caracteres. Algo que chame atenção do leitor. Mais pra frente, isso será utilizado na integração com twitter"
+		# end
 
-		configure :conteudo, :text do 
-			help "Aqui vem finalmente sua matéria completa. Sintaxe: **negrito**, _itálico_, . item de lista (pule uma linha para cada), . Item de lista ordenada, links: \#{link_to 'Texto do link', 'http://um.site.aqui'}" 
-		end
+		# configure :conteudo, :text do 
+		# 	help "Aqui vem finalmente sua matéria completa. Sintaxe: **negrito**, _itálico_, . item de lista (pule uma linha para cada), . Item de lista ordenada, links: \#{link_to 'Texto do link', 'http://um.site.aqui'}" 
+		# end
 
-		configure :imagem, :carrierwave 
+		# configure :imagem, :carrierwave 
 
 		edit do
 			field :coluna
